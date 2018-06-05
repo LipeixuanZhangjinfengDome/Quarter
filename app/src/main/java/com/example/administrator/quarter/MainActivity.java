47f7536b9771cd79d4ae2d5dcff149a779553bc0
Ntuj <595583576@qq.com>
2018-06-05 11:28:17 +0800

    侧滑（1）-张金凤

--- app/src/main/java/com/example/administrator/quarter/MainActivity.java
+++ app/src/main/java/com/example/administrator/quarter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.administrator.quarter;
 
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -10,11 +9,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.administrator.quarter.ui.Recommend.RecommendFragment;
 import com.example.administrator.quarter.ui.base.BaseActivity;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 /*lipeixuan=李佩璇*/
 public class MainActivity extends BaseActivity {
@@ -24,6 +24,8 @@ public class MainActivity extends BaseActivity {
     private ImageView mChuangzuo;
     private LinearLayout mLay;
     private FrameLayout mFlout;
+    private SimpleDraweeView my_head;
+    private TextView my_name;
     /**
      * 推荐
      */
@@ -37,6 +39,7 @@ public class MainActivity extends BaseActivity {
      */
     private RadioButton mBtn3;
     private RadioGroup mRg;
+    private SlidingMenu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +51,43 @@ public class MainActivity extends BaseActivity {
         fragmentManager.beginTransaction().replace(R.id.flout, recommendFragment).commit();
         //设置点击事件
         setLisenter();
+        //侧滑菜单
+        getMenu();
     }
 
-    private void setLisenter() {
+    private void getMenu() {
+        menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        //滑动显示的布局文件
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //触摸哪里可以发生滑动
+        menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+        //屏幕宽度
+        int v = (int) (getResources().getDisplayMetrics().widthPixels * 0.66);
+        //侧滑菜单和主界面相融合
+        menu.setBehindWidth(v);
+        //侧滑菜单显示的宽度
+        menu.setFadeDegree(0.35f);
+
+        //为侧滑菜单设置布局
+        /* menu.setMenu(R.layout.sliding_left);*/
+        menu.setMenu(R.layout.sliding_left);
+        my_head = menu.findViewById(R.id.my_head);
+        my_name = menu.findViewById(R.id.my_name);
+        /* my_head.setOnClickListener(this);*/
+    }
+
+    public void setLisenter() {
+        mNameImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //打开侧滑菜单
+                menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);//全屏滑出
+                menu.showMenu();
+                menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);//不可以滑出,只可以点击
+            }
+        });
+
     }
 
     private void initView() {
