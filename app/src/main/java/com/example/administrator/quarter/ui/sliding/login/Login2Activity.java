1c0839e8ce7df731be569a2855f0770e77e8a08d
Ntuj <595583576@qq.com>
2018-06-05 20:12:01 +0800

    sidrary

---
+++ app/src/main/java/com/example/administrator/quarter/ui/sliding/login/Login2Activity.java
@@ -0,0 +1,16 @@
+
+package com.example.administrator.quarter.ui.sliding.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.quarter.R;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
